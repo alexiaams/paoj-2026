f0f4930c83fd7f0d39dd7e1f4ac36951615f8190
alexiaams <alexiaseitan@yahoo.com>
2026-03-09 08:31:28 +0200

    lab1

--- src/com/pao/laboratory01/comparators/Podcast.java
+++ src/com/pao/laboratory01/comparators/Podcast.java
@@ -7,13 +7,39 @@ package com.pao.laboratory01.comparators;
 // 3. un Comparator extern (PodcastLengthComparator) — sortare dupa durata
 // 4. o metoda main in care cream cateva podcast-uri si le sortam in ambele moduri
 
-public class Podcast {
+
+import java.util.Comparator;
+
+public class Podcast implements Comparable<Podcast> {
 
     // TODO: adauga atributele: title (String), durationInSeconds (int)
+    private String title;
+    private int durationInSeconds;
+
     // TODO: adauga constructor cu ambele atribute
+    public Podcast(String title, int durationInSeconds)
+    {
+        this.title = title;
+        this.durationInSeconds = durationInSeconds;
+    }
     // TODO: suprascrie toString()
+    @Override
+    public String toString()
+    {
+        return "Podcast{" + "title =' " + title + '\'' + ", duration in seconds = " + durationInSeconds + '}';
+    }
+
     // TODO: implementeaza Comparable<Podcast> si suprascrie compareTo (dupa titlu)
+    @Override
+    public int compareTo(Podcast p)
+    {
+        //lexicografic dupa titlu
+        return this.title.compareTo(p.title);
+    }
+
     // TODO: adauga getter pentru durationInSeconds (necesar pentru comparator)
+
+    public int getDurationInSeconds() {return durationInSeconds;}
 
     // Metoda main — codul final care trebuie sa functioneze dupa implementare.
     // Ruleaza-l ca sa verifici ca totul e corect!
@@ -37,12 +63,21 @@ public class Podcast {
 
         // 3. sortare cu lambda — dupa durata descrescator
         java.util.Arrays.sort(podcasts,
-                (p1, p2) -> -1 // TODO trebuie -1 cand vrem sa afisam p2, p1 in ordinea finala, +1 daca p1, p2 e buna, 0 daca sunt egale.
+            (p1, p2) -> p2.getDurationInSeconds() - p1.getDurationInSeconds()
                 );
         System.out.println("Sortate dupa durata (descrescator, lambda):");
         System.out.println(java.util.Arrays.toString(podcasts));
     }
-}
+
+    }
+
 
 // TODO: creeaza o clasa PodcastLengthComparator care implementeaza Comparator<Podcast>
+class PodcastLengthComparator implements Comparator<Podcast> {
+    @Override
+    public int compare(Podcast p1, Podcast p2)
+    {
+        return p1.getDurationInSeconds() - p2.getDurationInSeconds();
+    }
+}
 //  si compara dupa durationInSeconds (vezi AudioBookLengthComparator ca model)
